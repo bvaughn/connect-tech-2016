5b569ced78ce90b5536922483bed39db175d8fc2
Brian Vaughn <brian.david.vaughn@gmail.com>
2016-09-30 16:33:44 -0400

    Added Building Blocks slide

--- src/Slides/06-the-building-blocks.js
+++ src/Slides/06-the-building-blocks.js
@@ -1,10 +1,80 @@
 import React from 'react';
-import Slide from './Components/Slide'
+import times from 'lodash.times';
+import Slide from './Components/Slide';
+import Stepper from './Components/Stepper';
+import './06-the-building-blocks.css';
 
 export default () => (
-  <Slide>
-    <h1>
-      The building blocks: Grid & Collection
-    </h1>
-  </Slide>
+  <Stepper numSteps={5}>
+    {(index) => (
+      <Slide>
+        <h1>
+          The building blocks: Grid & Collection
+        </h1>
+
+        <div className='Slides__BuildingBlocks'>
+          {index >= 1 && (
+            <div className='Slides__BuildingBlocks__Block'>
+              <h3>Grid</h3>
+              <div className='Slides__BuildingBlocks__Frame'>
+                {times(9).map((index) => (
+                  <div className='Slides__BuildingBlocks__Grid__Tile'>
+                    {index + 1}
+                  </div>
+                ))}
+              </div>
+            </div>
+          )}
+
+          {index >= 2 && (
+            <div className='Slides__BuildingBlocks__Block'>
+              <h3>List</h3>
+              <div className='Slides__BuildingBlocks__Frame'>
+                {times(4).map((index) => (
+                  <div className='Slides__BuildingBlocks__List__Tile'>
+                    {index + 1}
+                  </div>
+                ))}
+              </div>
+            </div>
+          )}
+
+          {index >= 3 && (
+            <div className='Slides__BuildingBlocks__Block'>
+              <h3>Table</h3>
+              <div className='Slides__BuildingBlocks__Frame'>
+                {times(4).map((rowIndex) => (
+                  <div className='Slides__BuildingBlocks__Table__Row'>
+                    {times(2).map((colIndex) => (
+                      <div className='Slides__BuildingBlocks__Table__Column'>
+                        {rowIndex + 1}
+                        {colIndex === 0 ? 'a' : 'b'}
+                      </div>
+                    ))}
+                  </div>
+                ))}
+              </div>
+            </div>
+          )}
+
+          {index >= 4 && (
+            <div className='Slides__BuildingBlocks__Block'>
+              <h3>Collection</h3>
+              <div className='Slides__BuildingBlocks__Frame'>
+                <div className='Slides__BuildingBlocks__Collection__Tile Slides__BuildingBlocks__Collection__Tile__1'>
+                  1
+                </div>
+                <div className='Slides__BuildingBlocks__Collection__Tile Slides__BuildingBlocks__Collection__Tile__2'>
+                  2
+                </div>
+                <div className='Slides__BuildingBlocks__Collection__Tile Slides__BuildingBlocks__Collection__Tile__3'>
+                  3
+                </div>
+              </div>
+            </div>
+          )}
+        </div>
+      </Slide>
+    )}
+  </Stepper>
 );
