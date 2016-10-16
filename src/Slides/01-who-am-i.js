cccf47bc6adc4eab6030c1eb41bda014a3d4b526
bvaughn <brian.david.vaughn@gmail.com>
2016-10-16 12:40:18 -0700

    Added logos to the who-am-i slide

--- src/Slides/01-who-am-i.js
+++ src/Slides/01-who-am-i.js
@@ -2,14 +2,29 @@ import React from 'react';
 import Slide from '../Components/Slide';
 import Step from '../Components/Step';
 import Stepper from '../Components/Stepper';
+import logoRosettaStone from '../../public/logo-rosetta-stone.jpg';
+import logoGoogle from '../../public/logo-google.jpg';
+import logoTreasureData from '../../public/logo-treasure-data.jpg';
+import logoFacebook from '../../public/logo-facebook.png';
+import './01-who-am-i.css';
 
 export default () => (
   <Stepper numSteps={5}>
     <Slide>
       <h1>Who am I?</h1>
       <ul>
-        <Step><li>Writing JavaScript professionally for 15 years</li></Step>
-        <Step><li>Worked some cool places: Rosetta Stone, Google, Treasure Data, Facebook</li></Step>
+        <Step><li>Written JavaScript professionally for 15 years</li></Step>
+        <Step>
+          <li>
+            I've worked some cool places:
+            <div className='WhoAmILogos'>
+              <img role='presentation' className='WhoAmILogo' src={logoRosettaStone} />
+              <img role='presentation' className='WhoAmILogo' src={logoGoogle} />
+              <img role='presentation' className='WhoAmILogo' src={logoTreasureData} />
+              <img role='presentation' className='WhoAmILogo' src={logoFacebook} />
+            </div>
+          </li>
+        </Step>
         <Step><li>I <i className='fa fa-heart' style={{ color: '#f92672' }} /> open source (<a href="https://github.com/bvaughn/">github.com/bvaughn</a>)</li></Step>
         <Step><li>Passionate about performance and architecture</li></Step>
       </ul>
