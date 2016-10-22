14061c1f28f32ca661b5f3cbb6cc5cc8bcc89aee
Brian Vaughn <bvaughn@fb.com>
2016-10-22 10:03:35 -0400

    Ordering tweak

--- src/Slides/04d-avoid-unnecessary-renders.js
+++ src/Slides/04d-avoid-unnecessary-renders.js
@@ -4,7 +4,7 @@ import Step from '../Components/Step';
 import Stepper from '../Components/Stepper';
 
 export default () => (
-  <Stepper numSteps={4}>
+  <Stepper numSteps={7}>
     <Slide>
       <h1>Avoid unnecessary renders</h1>
       <h2>Is that all?</h2>
@@ -12,12 +12,12 @@ export default () => (
         <Step><li><a href='https://facebook.github.io/immutable-js/'>Immutable</a> data speeds up comparisons</li></Step>
         <Step>
           <li>
-            Choose <code>props</code> carefully <Step index={3}><span>(also helps with testing!)</span></Step>
+            Choose <code>props</code> carefully <Step index={6}><span>(also helps with testing!)</span></Step>
 
             <ul>
-              <li><i className='fa fa-frown-o' /> users</li>
-              <li><i className='fa fa-meh-o' /> user</li>
-              <li><i className='fa fa-smile-o' /> name</li>
+              <Step index={3}><li><i className='fa fa-frown-o' /> <code>users</code>, <code>index</code></li></Step>
+              <Step index={4}><li><i className='fa fa-smile-o' /> <code>user</code></li></Step>
+              <Step index={5}><li><i className='fa fa-smile-o' /><i className='fa fa-smile-o' /> <code>name</code>, <code>email</code></li></Step>
             </ul>
           </li>
         </Step>
